4312c20ce44f1edb0fdda779deb8430a69c3d332
amitroy4 <amitroyrock6071@gmail.com>
2023-02-25 23:02:28 +0600

    change extension .js to .jsx

--- src/components/work/Work.jsx
+++ src/components/work/Work.jsx
@@ -16,10 +16,10 @@ const Work = () => {
                     </div>
                     <div className="right">
                         <div className="box">
-                            <WorkLayout title = "Strategy" photolink = "images/work/work1.png" para = "Euismod faucibus turpis eu gravida mi. Pellentesque et velit aliquam ."/>
-                            <WorkLayout title = "Wireframing" photolink = "images/work/work2.png" para = "Euismod faucibus turpis eu gravida mi. Pellentesque et velit aliquam ."/>
-                            <WorkLayout title = "Design" photolink = "images/work/work3.png" para = "Euismod faucibus turpis eu gravida mi. Pellentesque et velit aliquam ."/>
-                            <WorkLayout title = "Development" photolink = "images/work/work4.png" para = "Euismod faucibus turpis eu gravida mi. Pellentesque et velit aliquam ."/>
+                            <WorkLayout title="Strategy" photolink="images/work/work1.png" para="Euismod faucibus turpis eu gravida mi. Pellentesque et velit aliquam ." />
+                            <WorkLayout title="Wireframing" photolink="images/work/work2.png" para="Euismod faucibus turpis eu gravida mi. Pellentesque et velit aliquam ." />
+                            <WorkLayout title="Design" photolink="images/work/work3.png" para="Euismod faucibus turpis eu gravida mi. Pellentesque et velit aliquam ." />
+                            <WorkLayout title="Development" photolink="images/work/work4.png" para="Euismod faucibus turpis eu gravida mi. Pellentesque et velit aliquam ." />
                         </div>
                     </div>
                 </div>
